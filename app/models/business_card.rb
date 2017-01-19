1348557c30c11133255b06a0a16efaa0f8dd277d
Steve <stevenjchang@gmail.com>
2017-01-19 19:55:52 +0800

    scaffold

---
+++ app/models/business_card.rb
@@ -0,0 +1,2 @@
+class BusinessCard < ApplicationRecord
+end
---
+++ db/migrate/20170119115504_create_business_cards.rb
@@ -0,0 +1,11 @@
+class CreateBusinessCards < ActiveRecord::Migration[5.0]
+  def change
+    create_table :business_cards do |t|
+      t.string :name
+      t.string :address
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
