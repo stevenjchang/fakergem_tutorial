e6a7646d6f137f6901a953e6c2978db10fd6a99e
Steve <stevenjchang@gmail.com>
2017-01-19 20:43:36 +0800

    created db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  BusinessCard.create([{
+    name: Faker::Name.name,
+    address: Faker::Address.street_address,
+    phone: Faker::PhoneNumber.cell_phone
+    }])
+end
